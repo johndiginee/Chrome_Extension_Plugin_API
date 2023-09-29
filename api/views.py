80d6970ff839b8558450ad4ac9b3171aed5ae3a5
John Diginee <Johndbizz@gmail.com>
2023-09-29 10:24:35 +0100

    Chore: Fix bug

--- api/views.py
+++ api/views.py
@@ -8,9 +8,11 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.core.exceptions import ValidationError
 import os
+from django.urls import reverse
 
 @csrf_exempt
 def upload_video(request):
+    """Upload video."""
     if request.method == 'POST':
         video_file = request.FILES.get('video_file')
         if video_file:
@@ -21,21 +23,17 @@ def upload_video(request):
                 return JsonResponse({'error': 'Invalid file type. Only video files are allowed.'}, status=400)
 
             try:
-                Video.objects.create(video_file=video_file)
-                return JsonResponse({'message': 'Video uploaded successfully.'})
+                # Save the video and reverse the url
+                video = Video.objects.create(video_file=video_file)
+                video_url = request.build_absolute_uri(video.video_file.url)
+                return JsonResponse({'message': 'Video uploaded successfully.', 'video_url': video_url})
             except ValidationError:
                 return JsonResponse({'error': 'Invalid file format.'}, status=400)
     return JsonResponse({'error': 'Invalid request'})
 
-def play_video(request, video_id):
-    try:
-        video = Video.objects.get(pk=video_id)
-        return render(request, 'play_video.html', {'video': video})
-    except Video.DoesNotExist:
-        return JsonResponse({'error': 'Video not found'}, status=404)
-
 @api_view(['GET'])
 def list_videos(request):
+    """List all videos."""
     if request.method == 'GET':
         videos = Video.objects.all()
         serializer = VideoSerializer(videos, many=True)
