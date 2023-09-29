57bec3fd9bef70b713a1ca9b9902e33b8de5fefa
John Diginee <Johndbizz@gmail.com>
2023-09-29 14:33:10 +0100

    Chore: Update model

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 class Video(models.Model):
     video_file = models.FileField(upload_to='videos/')
-    timestamp = models.DateTimeField(auto_now_add=True)
+    # timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
